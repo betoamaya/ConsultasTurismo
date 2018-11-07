2a75e7582f43dae84d45f156f23d8e21e12eace3
Roberto Amaya <betoamaya@msn.com>
2018-11-07 11:11:49 -0600

    SG 575402

--- Intelisis/Scripts Soporte/Gasto - Corregir Fecha.sql
+++ Intelisis/Scripts Soporte/Gasto - Corregir Fecha.sql
@@ -4,15 +4,20 @@ FROM dbo.Gasto AS g
     INNER JOIN dbo.GastoD AS gd
         ON gd.ID = g.ID
 WHERE g.Mov = 'Solicitud Gasto'
-      AND g.MovID = '575355'
+      AND g.MovID = '575402'
       AND g.Usuario = 'SITTI';
 
 GO
 BEGIN TRAN;
 UPDATE dbo.GastoD
-SET Fecha = '2018-10-28 00:00:00.000'
-WHERE ID = 226303
-      AND Renglon = 2048;
+SET Fecha = '2018-10-08 00:00:00.000'
+WHERE ID = 226412
+      AND Renglon IN (38912,43008);
+
+UPDATE dbo.GastoD
+SET Fecha = '2018-10-05 00:00:00.000'
+WHERE ID = 226412
+      AND Renglon IN (40960,45056);
 /*
 COMMIT;
 ROLLBACK;

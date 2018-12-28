e07636b333009bcba9e44208bac38a887c740397
Roberto Amaya <betoamaya@msn.com>
2018-12-28 12:33:24 -0600

    Consultas Actualizadas

---
+++ Intelisis/Consultas GtpIntelisisDB/Gasto - Corregir Fecha.sql
@@ -0,0 +1,24 @@
+/*Actualizar Fecha Renglon*/
+SELECT gd.*
+FROM dbo.Gasto AS g
+    INNER JOIN dbo.GastoD AS gd
+        ON gd.ID = g.ID
+WHERE g.Mov = 'Solicitud Gasto'
+      AND g.MovID = '575402'
+      AND g.Usuario = 'SITTI';
+
+GO
+BEGIN TRAN;
+UPDATE dbo.GastoD
+SET Fecha = '2018-10-08 00:00:00.000'
+WHERE ID = 226412
+      AND Renglon IN ( 38912, 43008 );
+
+UPDATE dbo.GastoD
+SET Fecha = '2018-10-05 00:00:00.000'
+WHERE ID = 226412
+      AND Renglon IN ( 40960, 45056 );
+/*
+COMMIT;
+ROLLBACK;
+*/
---
+++ Intelisis/Consultas GtpIntelisisDB/Tesoreria - Error Control de Ingresos.sql
@@ -0,0 +1,71 @@
+/*Buscando los parametros del error en log*/
+
+SELECT *
+FROM dbo.Interfaz_Logs AS il
+WHERE il.SP = 'Interfaz_tesoreriaDepositarDetalle'
+ORDER BY il.Fecha DESC;
+
+/*Parametros
+<Parametros>
+  <Empresa>TUN  </Empresa>
+  <FechaDeCorte>2018-10-31T00:00:00</FechaDeCorte>
+  <Concepto>VIAJES ESPECIALES</Concepto>
+  <Moneda>Pesos     </Moneda>
+  <TipoCambio>1.000000000000000e+000</TipoCambio>
+  <Usuario>SITTI     </Usuario>
+  <Referencia>1810310P13010196</Referencia>
+  <CtaDinero>BBV-67    </CtaDinero>
+  <Importe>7650.0000</Importe>
+  <CentroDeCostos>P1301     </CentroDeCostos>
+  <Observaciones># Bolsa :</Observaciones>
+  <Comentarios>Corte registrado el: martes, 6 de noviembre de 2018 a las 05:51:23 p. m.</Comentarios>
+  <Partidas><row><fila IdIntelisis="464508" MovID="1611"/></row></Partidas>
+</Parametros>
+*/
+
+--Error al aplicar el movimiento de dep?sito de Intelisis: Error30230, Mensaje = 
+/*Buscar mensaje de error*/
+
+SELECT *
+FROM dbo.MensajeLista AS ml
+WHERE ml.Mensaje = '30230';
+
+/*30230	No corresponde la aplicación con el importe del Movimiento	ERROR	0*/
+
+/*Revisando el saldo que genero esa factura*/
+
+SELECT c.Importe + c.Impuestos,
+       *
+FROM dbo.Cxc AS c
+WHERE c.ID = 464508;
+
+--7650.00	464508	TUN  	Cobro Otros Ingresos	1611
+
+SELECT din.ID,
+       din.MovID,
+       din.Saldo,
+       din.Observaciones,
+       din.Referencia,
+       din.Cliente,
+       din.Estatus
+FROM dbo.MovFlujo mf
+    INNER JOIN dbo.Dinero din
+        ON mf.DID = din.ID
+WHERE mf.DModulo = 'DIN'
+      AND mf.OModulo IN ( 'CXC', 'VTAS' )
+      AND mf.OID = 464508
+      AND mf.OMovID = '1611';
+--Resultado
+--===========================================================================
+--ID		MovID	Saldo	Observaciones	Referencia	Cliente	Estatus
+--1674208	1543599	NULL	NULL			GPO KIA		NULL	CONCLUIDO
+--===========================================================================
+
+/* Buscar si fue concluido por la interfaz*/
+SELECT *
+FROM dbo.Interfaz_Logs AS il
+WHERE il.SP = 'Interfaz_tesoreriaDepositarDetalle'
+      AND CAST(il.Parametros AS VARCHAR(MAX)) LIKE '%1810310P13010196%'
+      AND il.Fecha
+      BETWEEN GETDATE() - 2 AND GETDATE()
+ORDER BY il.Fecha DESC;
